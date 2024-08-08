abb713bee0448da8e2c1d5f21f6d43950faf7759
lzx <lzx@dareway.com.cn>
2024-08-08 10:20:31 +0800

    react16-memleak

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { useState, useEffect, useRef } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "antd/dist/antd.css";
+import { Button } from "antd";
+import { Modal } from "antd";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BigData() {
+  const [data] = useState(new Uint8Array(1024 * 1024 * 300));
+  const inputref = useRef(null);
+
+  useEffect(() => {
+    inputref.current.focus();
+  }, []);
+
+  return (
+    <div>
+      <h1>BigData length: {data.length}</h1>
+      <input ref={inputref}></input>
+    </div>
+  );
+}
+
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+  return (
+    <div>
+      <Button type="primary" onClick={() => setIsModalOpen(true)}>
+        Open Modal
+      </Button>
+
+      {isModalOpen && (
+        <Modal
+          title="Modal"
+          open={true}
+          onOk={() => setIsModalOpen(false)}
+          onCancel={() => setIsModalOpen(false)}
+        >
+          <BigData></BigData>
+        </Modal>
+      )}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render();
